5a63169202d2827cb54ed7adbe4f4c5aeb778622
Mac Rowe <m.rowe42@gmail.com>
2021-03-01 12:53:34 -0500

    commit tree 267e845ff75de0d6cb043741762234fd16a9bac4

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+
+
+class Dog
+
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed 
+    end 
+
+end
+
